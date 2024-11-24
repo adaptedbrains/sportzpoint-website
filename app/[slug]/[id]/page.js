7d01520a83a9506439c7ad9a0707245a61a9c727
manashbarai <manashjbarai@gmail.com>
2024-11-25 02:13:16 +0530

    include live blof with state

--- app/[slug]/[id]/page.js
+++ app/[slug]/[id]/page.js
@@ -1,19 +1,20 @@
 'use client';
-import { useEffect, useState } from 'react';
 
+import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
+
 import BlogPost from '@/components/BlogPost';
 import LoginSignUp from '@/components/LoginSignUp';
 import Follow from '@/components/Follow';
 import FeaturedEvents from '@/components/FeaturedEvents';
 import { BlinkBlur } from 'react-loading-indicators';
-import LatestStories from "@/components/LatestStory";
+import LatestStories from '@/components/LatestStory';
 import WebStoriesJson from '@/components/WebstoeyJson';
 
 const BlogPage = () => {
     const pathname = usePathname();
-    const id = pathname.split("/")[2]; // Extract the slug from the path
-
+    const id = pathname.split('/')[2]; // Extract the slug from the path
+    const [livBlog, setLivBlog] = useState([]);
     const [post, setPost] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
 
@@ -23,7 +24,10 @@ const BlogPage = () => {
                 try {
                     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/article/slug/${id}`);
                     const data = await response.json();
-                    setPost(data); // Assuming API returns a single post object
+                    if (data.article?.live_blog_updates?.length) {
+                        setLivBlog(data.article.live_blog_updates);
+                    }
+                    setPost(data);
                 } catch (error) {
                     console.error('Error fetching post:', error);
                 } finally {
@@ -39,29 +43,44 @@ const BlogPage = () => {
         if (!post || !post.article) return null;
 
         switch (post.article.type) {
-            case "Web Story":
+            case 'Web Story':
+                return <WebStoriesJson post={post.article} />;
+
+            case 'LiveBlog':
                 return (
-                    <WebStoriesJson post={post.article}/>
+                    <div>
+                        <BlogPost postData={post.article} />
+                        <div className="grid grid-cols-5 justify-between items-center mb-5">
+                            <div className="bg-green-800 h-[1px] col-span-2"></div>
+                            <p className="border col-span-1 border-green-800 text-center px-2">Live Updates</p>
+                            <div className="bg-green-800 h-[1px] col-span-2"></div>
+                        </div>
+
+                        <div className="rounded  bg-white">
+                            {livBlog.map((live, i) => (
+                                <div key={i} className=" p-4 flex flex-col">
+                                    <h1 className="font-semibold text-xl pb-2">{live.title}</h1>
+                                    <hr/>
+                                    <div dangerouslySetInnerHTML={{ __html: live.content }} />
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 );
-            // case "Live Blog":
-            //     return (
-            //         <div>
-            //             <h1 className="text-2xl font-bold">Live Blog</h1>
-            //             <p>{post.article.content}</p>
-            //         </div>
-            //     );
+
             default:
                 return (
                     <div>
                         <BlogPost postData={post.article} />
-                        <div className='grid grid-cols-5 justify-between items-center mb-5'>
-                            <div className='bg-green-800 h-[1px] col-span-2'></div>
-                            <p className='border col-span-1 border-green-800 text-center px-2'>Next Article</p>
-                            <div className='bg-green-800 h-[1px] col-span-2'></div>
+                        <div className="grid grid-cols-5 justify-between items-center mb-5">
+                            <div className="bg-green-800 h-[1px] col-span-2"></div>
+                            <p className="border col-span-1 border-green-800 text-center px-2">Next Article</p>
+                            <div className="bg-green-800 h-[1px] col-span-2"></div>
                         </div>
-                        {post.latestArticles.map((p, index) => (
-                            <BlogPost postData={p} key={p._id || index} />
-                        ))}
+                        {livBlog.length !== 0 &&
+                            livBlog.map((p, index) => (
+                                <BlogPost postData={p} key={p._id || index} />
+                            ))}
                     </div>
                 );
         }
@@ -79,7 +98,7 @@ const BlogPage = () => {
             {/* Main Content */}
             <div className="lg:col-span-5 col-span-1">
                 {isLoading ? (
-                    <div className='flex justify-center mt-20'>
+                    <div className="flex justify-center mt-20">
                         <BlinkBlur color="#32cd32" size="medium" />
                     </div>
                 ) : (
