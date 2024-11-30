cb0c9578e5cb468881d8ccbbd15ff5ac4f00cc51
Oliur Sahin <84772210+oliursahin@users.noreply.github.com>
2024-12-01 01:57:48 +0530

    chore: removing publive cdn urls

--- util/headerScore.js
+++ util/headerScore.js
@@ -68,17 +68,17 @@ export const dummyStory = [
     id:1,
     pages: [
         {
-            image: "https://img-cdn.thepublive.com/fit-in/0x0/filters:format(webp)/sportzpoint/media/media_files/AybZN5lY5dRqUB7flruv.jpg",
+            image: "https://dmpsza32x691.cloudfront.net/sportzpoint/media/media_files/AybZN5lY5dRqUB7flruv.jpg",
             heading: "Welcome to Web Stories",
             description: "Learn how to create immersive stories for the web.",
         },
         {
-            image: "https://img-cdn.thepublive.com/fit-in/0x0/filters:format(webp)/sportzpoint/media/media_files/AybZN5lY5dRqUB7flruv.jpg",
+            image: "https://dmpsza32x691.cloudfront.net/sportzpoint/media/media_files/AybZN5lY5dRqUB7flruv.jpg",
             heading: "Add Visual Appeal",
             description: "Use high-quality images and videos to tell your story.",
         },
         {
-            image: "https://img-cdn.thepublive.com/fit-in/0x0/filters:format(webp)/sportzpoint/media/media_files/AybZN5lY5dRqUB7flruv.jpg",
+            image: "https://dmpsza32x691.cloudfront.net/sportzpoint/media/media_files/AybZN5lY5dRqUB7flruv.jpg",
             heading: "Engage Your Audience",
             description: "Add interactive elements like links, quizzes, and polls.",
         },
@@ -90,17 +90,17 @@ export const dummyStory = [
     id:2,
     pages: [
         {
-            image: "https://img-cdn.thepublive.com/fit-in/0x0/filters:format(webp)/sportzpoint/media/media_files/JkmIykcr87u2E5zCV0dV.png",
+            image: "https://dmpsza32x691.cloudfront.net/sportzpoint/media/media_files/JkmIykcr87u2E5zCV0dV.png",
             heading: "Welcome to Web Stories",
             description: "Learn how to create immersive stories for the web.",
         },
         {
-            image: "https://img-cdn.thepublive.com/fit-in/0x0/filters:format(webp)/sportzpoint/media/media_files/JkmIykcr87u2E5zCV0dV.png",
+            image: "https://dmpsza32x691.cloudfront.net/sportzpoint/media/media_files/JkmIykcr87u2E5zCV0dV.png",
             heading: "Add Visual Appeal",
             description: "Use high-quality images and videos to tell your story.",
         },
         {
-            image: "https://img-cdn.thepublive.com/fit-in/0x0/filters:format(webp)/sportzpoint/media/media_files/JkmIykcr87u2E5zCV0dV.png",
+            image: "https://dmpsza32x691.cloudfront.net/sportzpoint/media/media_files/JkmIykcr87u2E5zCV0dV.png",
             heading: "Engage Your Audience",
             description: "Add interactive elements like links, quizzes, and polls.",
         },
@@ -112,17 +112,17 @@ export const dummyStory = [
     id:3,
     pages: [
         {
-            image: "https://img-cdn.thepublive.com/fit-in/0x0/filters:format(webp)/sportzpoint/media/media_files/AybZN5lY5dRqUB7flruv.jpg",
+            image: "https://dmpsza32x691.cloudfront.net/sportzpoint/media/media_files/AybZN5lY5dRqUB7flruv.jpg",
             heading: "Welcome to Web Stories",
             description: "Learn how to create immersive stories for the web.",
         },
         {
-            image: "https://img-cdn.thepublive.com/fit-in/0x0/filters:format(webp)/sportzpoint/media/media_files/AybZN5lY5dRqUB7flruv.jpg",
+            image: "https://dmpsza32x691.cloudfront.net/sportzpoint/media/media_files/AybZN5lY5dRqUB7flruv.jpg",
             heading: "Add Visual Appeal",
             description: "Use high-quality images and videos to tell your story.",
         },
         {
-            image: "https://img-cdn.thepublive.com/fit-in/0x0/filters:format(webp)/sportzpoint/media/media_files/AybZN5lY5dRqUB7flruv.jpg",
+            image: "https://dmpsza32x691.cloudfront.net/sportzpoint/media/media_files/AybZN5lY5dRqUB7flruv.jpg",
             heading: "Engage Your Audience",
             description: "Add interactive elements like links, quizzes, and polls.",
         },
@@ -133,7 +133,7 @@ export const dummyStory = [
 export const CricketArticles = [
     {
       imageUrl:
-        "https://img-cdn.thepublive.com/fit-in/580x348/filters:format(webp)/sportzpoint/media/media_files/2024/11/18/XzkyOoZeAsEcSaOAx7rG.webp",
+        "https://dmpsza32x691.cloudfront.net/sportzpoint/media/media_files/2024/11/18/XzkyOoZeAsEcSaOAx7rG.webp",
       category: "Cricket",
       title: "Syed Mushtaq Ali Trophy 2024: Mohammed Shami to play for Bengal...",
       author: "Koushik Biswas",
@@ -142,7 +142,7 @@ export const CricketArticles = [
     },
     {
         imageUrl:
-          "https://img-cdn.thepublive.com/fit-in/580x348/filters:format(webp)/sportzpoint/media/media_files/2024/11/18/XzkyOoZeAsEcSaOAx7rG.webp",
+          "https://dmpsza32x691.cloudfront.net/sportzpoint/media/media_files/2024/11/18/XzkyOoZeAsEcSaOAx7rG.webp",
         category: "Cricket",
         title: "Syed Mushtaq Ali Trophy 2024: Mohammed Shami to play for Bengal...",
         author: "Koushik Biswas",
@@ -151,7 +151,7 @@ export const CricketArticles = [
       },
       {
         imageUrl:
-          "https://img-cdn.thepublive.com/fit-in/580x348/filters:format(webp)/sportzpoint/media/media_files/2024/11/18/XzkyOoZeAsEcSaOAx7rG.webp",
+          "https://dmpsza32x691.cloudfront.net/sportzpoint/media/media_files/2024/11/18/XzkyOoZeAsEcSaOAx7rG.webp",
         category: "Cricket",
         title: "Syed Mushtaq Ali Trophy 2024: Mohammed Shami to play for Bengal...",
         author: "Koushik Biswas",
@@ -160,7 +160,7 @@ export const CricketArticles = [
       },
       {
         imageUrl:
-          "https://img-cdn.thepublive.com/fit-in/580x348/filters:format(webp)/sportzpoint/media/media_files/2024/11/18/XzkyOoZeAsEcSaOAx7rG.webp",
+          "https://dmpsza32x691.cloudfront.net/sportzpoint/media/media_files/2024/11/18/XzkyOoZeAsEcSaOAx7rG.webp",
         category: "Cricket",
         title: "Syed Mushtaq Ali Trophy 2024: Mohammed Shami to play for Bengal...",
         author: "Koushik Biswas",
@@ -169,7 +169,7 @@ export const CricketArticles = [
       },
       {
         imageUrl:
-          "https://img-cdn.thepublive.com/fit-in/580x348/filters:format(webp)/sportzpoint/media/media_files/2024/11/18/XzkyOoZeAsEcSaOAx7rG.webp",
+          "https://dmpsza32x691.cloudfront.net/sportzpoint/media/media_files/2024/11/18/XzkyOoZeAsEcSaOAx7rG.webp",
         category: "Cricket",
         title: "Syed Mushtaq Ali Trophy 2024: Mohammed Shami to play for Bengal...",
         author: "Koushik Biswas",
